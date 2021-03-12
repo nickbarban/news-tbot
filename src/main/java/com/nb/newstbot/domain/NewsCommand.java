75195782e798319e6b735ba4f6d43629e3887142
nbarban <nbarban@millhouse.com>
2021-03-12 11:40:39 +0200

    change log

--- src/main/java/com/nb/newstbot/domain/NewsCommand.java
+++ src/main/java/com/nb/newstbot/domain/NewsCommand.java
@@ -63,7 +63,7 @@ public class NewsCommand extends ServiceCommand {
                     log.info("{} messages will be sent", unsentArticles.size());
                     unsentArticles.forEach(a -> {
                         String message = prepareMessage(a);
-                        log.info("Send message: {} to {} chat{}", message, latestArticlePerChat.size(), latestArticlePerChat.size() == 1 ? "" : "s");
+                        log.info("Send message: {} to chat {}", message, chatId);
                         // TODO by nbarban: 09/03/21 Should be added possibility to send unread articles to each chat personally
                         sendAnswer(sender, chatId, commandIdentifier, username, message);
                     });
